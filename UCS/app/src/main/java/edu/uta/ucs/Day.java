a65d9ee4e87b411caffc6fb0b0e92016ae95ff1f
Arunk3001 <arunk3001@gmail.com>
2015-04-05 02:07:03 -0500

    Added data classes
    Created Section class and Days of week and Class Status enums to go with
    it

---
+++ UCS/app/src/main/java/edu/uta/ucs/Day.java
@@ -0,0 +1,8 @@
+package edu.uta.ucs;
+
+/**
+ * Created by arunk_000 on 4/5/2015.
+ */
+public enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
---
+++ UCS/app/src/main/java/edu/uta/ucs/Section.java
@@ -0,0 +1,12 @@
+package edu.uta.ucs;
+
+import java.util.ArrayList;
+
+/**
+ * Created by arunk_000 on 4/5/2015.
+ */
+public class Section {
+    String sectionNumber, instructor;
+    ClassStatus sectionStatus;
+    ArrayList<Day> days;
+}
