b3b06dd1a7dca4b4ba9a44fb955789ef1c1219d6
Arun Kalahasti <arunk3001@gmail.com>
2015-04-11 14:51:07 -0500

    Cleaned up code and added commentary

--- UCS/app/src/main/java/edu/uta/ucs/CreateAccount.java
+++ UCS/app/src/main/java/edu/uta/ucs/CreateAccount.java
@@ -7,9 +7,6 @@ import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -18,6 +15,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 
+/**
+ * This Activity is the input form for a user to create a new account
+ */
 public class CreateAccount extends ActionBarActivity {
     public static final String ACTION_CREATE_ACCOUNT ="edu.uta.ucs.intent.action.CREATE_ACCOUNT";
     private static final String SPOOF_ACCOUNT_CREATION = "{\"Success\":true,\"Email\":\"b@b.b\",\"Username\":\"b\",\"Message\":\"Account Added.\"}";
@@ -30,51 +30,46 @@ public class CreateAccount extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
+        // Register this receiver with LocalBroadcastManager
         LocalBroadcastManager.getInstance(this).registerReceiver(new CreateAccountReceiver(), new IntentFilter(ACTION_CREATE_ACCOUNT));
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_create_account, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
+    /**
+     * User requests account creation canceled
+     * fields are reset and activity closed
+     *
+     * @param view view from which activity is called
+     */
     public void cancelAccountCreation(View view){
+        // Reset all of the input fields
+        // *** MAY NOT BE NEEDED ***
         ((EditText) findViewById(R.id.create_account_email)).setText("");
         ((EditText) findViewById(R.id.create_account_username)).setText("");
         ((EditText) findViewById(R.id.create_account_password)).setText("");
         ((EditText) findViewById(R.id.create_account_confirm_password)).setText("");
+
+        // Close Activity
         finish();
     }
 
+    /**
+     * User requests account be created with given fields
+     *
+     * @param view view from which activity is called
+     */
     public void attemptAccountCreation(View view){
-
+        // Initiate some values
         View focusView = null;
         String url=null;
         boolean cancel = false;
 
+        // Get data from layout fields
         String email = ((EditText) findViewById(R.id.create_account_email)).getText().toString();
         String username = ((EditText) findViewById(R.id.create_account_username)).getText().toString();
         String password = ((EditText) findViewById(R.id.create_account_password)).getText().toString();
         String passwordConfirmation = ((EditText) findViewById(R.id.create_account_confirm_password)).getText().toString();
 
+        // Check valid
         if(!emailIsValid(email)){
             //Break and inform user
             focusView = ((EditText) findViewById(R.id.create_account_email));
@@ -83,6 +78,7 @@ public class CreateAccount extends ActionBarActivity {
 
         }
 
+        // Check valid
         if(!passwordsValid(password, passwordConfirmation)){
             //break and
             ((EditText) findViewById(R.id.create_account_password)).setText("");
@@ -91,35 +87,59 @@ public class CreateAccount extends ActionBarActivity {
             cancel = true;
         }
 
+        // Build url from user given fields
         url = CREATE_ACCOUNT_URL+CREATE_ACCOUNT_PARAMS[0]+username+CREATE_ACCOUNT_PARAMS[1]+password+CREATE_ACCOUNT_PARAMS[2]+email;
 
+        // Create activity intent
         Intent intent = new Intent(this, HTTPGetService.class);
+        // Listener filter for result broadcast
         intent.putExtra(HTTPGetService.SOURCE_INTENT, ACTION_CREATE_ACCOUNT);
+
+        // Last chance to interrupt before account creation is attempted
         if (cancel){
 
             focusView.requestFocus();
         }
         else {
+            // Spoof data switch
             if (true) {
+                // Put spoof request instead of url in intent extras
                 intent.putExtra(HTTPGetService.URL_REQUEST, HTTPGetService.SPOOF_SERVER);
                 intent.putExtra(HTTPGetService.SPOOFED_RESPONSE, SPOOF_ACCOUNT_CREATION);
             } else
+                // Put creation request in intent extras
                 intent.putExtra(HTTPGetService.URL_REQUEST, url);
 
+            // Launch service
             startService(intent);
         }
 
     }
 
+    /**
+     * Checks to make sure email is valid
+     *
+     * @param email email to be checked for validity
+     * @return
+     */
     private boolean emailIsValid(String email){
         return email.contains("@");
     }
 
+    /**
+     * Checks to make sure given passwords are valid
+     *
+     * @param pass password field - primary
+     * @param confirmPass password field - secondary
+     * @return
+     */
     private boolean passwordsValid(String pass, String confirmPass){
+        // Check to make sure conformation password matches password
         boolean match = pass.equals(confirmPass);
         if (!match){
             ((EditText) findViewById(R.id.create_account_password)).setError(getString(R.string.error_password_mismatch));
         }
+        // Ensure password meets length requirement
         boolean length = pass.length()>0;
         if (!length){
             ((EditText) findViewById(R.id.create_account_password)).setError(getString(R.string.error_invalid_password));
