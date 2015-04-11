a3b9cd5561bcc19f7587d60a42fc6ded3165c1c0
Arunk3001 <arunk3001@gmail.com>
2015-04-11 03:48:03 -0500

    Initial Commit

--- UCS/app/src/main/java/edu/uta/ucs/SelectCourses.java
+++ UCS/app/src/main/java/edu/uta/ucs/SelectCourses.java
@@ -1,19 +1,44 @@
 package edu.uta.ucs;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class SelectCourses extends ActionBarActivity {
+
+    public static final String ACTION_DEPARTMENT_SELECT ="edu.uta.ucs.intent.action.ACTION_DEPARTMENT_SELECT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_courses);
+
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(new DepartmentCoursesReceiver(), new IntentFilter(ACTION_DEPARTMENT_SELECT));
     }
 
+    private void getDepartmentCourses(){
+        String url = null;
+        Intent intent = new Intent(this, HTTPGetService.class);
+        if(true) {
+
+            intent.putExtra(HTTPGetService.URL_REQUEST, HTTPGetService.SPOOF_SERVER);
+            intent.putExtra(HTTPGetService.SPOOFED_RESPONSE, SPOOFED_DEPARTMENT_COURSES);
+        }
+        else
+            intent.putExtra(HTTPGetService.URL_REQUEST, url);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +60,28 @@ public class SelectCourses extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class DepartmentCoursesReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            JSONObject response = null;
+            boolean success = false;
+            try {
+                response = new JSONObject(intent.getStringExtra(HTTPGetService.SERVER_RESPONSE));
+                success = response.getBoolean("Success");
+                if(success){
+                    Toast.makeText(getApplicationContext(), "Account Created", Toast.LENGTH_LONG).show();
+                    Intent launchMainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                    getApplicationContext().startActivity(launchMainActivity);
+                }
+                else {
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
