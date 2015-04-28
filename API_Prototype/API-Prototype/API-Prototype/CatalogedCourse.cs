6664d2319deae7dac56ffba289d5c9b09bd33869
Peyton Casper <peytoncas@gmail.com>
2015-04-28 09:45:41 -0500

    Added API Backend to the Project

---
+++ API_Prototype/API-Prototype/API-Prototype/CatalogedCourse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArchosAPI
+{
+    public class CatalogedCourse
+    {
+        public int Id { get; set; }
+        public int DepartmentId { get; set; }
+        public string CourseName { get; set; }
+        public string CourseNumber { get; set; }
+    }
+}
---
+++ API_Prototype/API-Prototype/API-Prototype/Models/UTASearchResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Test;
+
+namespace ArchosAPI.Models
+{
+    public class UTASearchResult
+    {
+        public string Semester { get; set; }
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public List<UTAClass> CourseResults { get; set; } 
+    }
+}
