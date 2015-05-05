3a39cea63efe8faf1e335cf041ce0825c8a16b98
Arunk3001 <arunk3001@gmail.com>
2015-05-05 00:43:03 -0500

    Main Activity onResume
    Added onResume

--- UCS/app/src/main/java/edu/uta/ucs/MainActivity.java
+++ UCS/app/src/main/java/edu/uta/ucs/MainActivity.java
@@ -84,6 +84,12 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        Schedule.loadSchedulesFromFile(MainActivity.this);
+    }
+
+    @Override
     protected void onPause() {
         super.onPause();
     }
