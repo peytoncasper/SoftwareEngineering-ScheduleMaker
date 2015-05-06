380248615d775934379c8a219de027b39b414c1b
Arunk3001 <arunk3001@gmail.com>
2015-05-06 04:05:50 -0500

    Added hidden delete button to section adapter

--- UCS/app/src/main/java/edu/uta/ucs/MySectionArrayAdapter.java
+++ UCS/app/src/main/java/edu/uta/ucs/MySectionArrayAdapter.java
@@ -1,11 +1,14 @@
 package edu.uta.ucs;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,15 +17,9 @@ import java.util.ArrayList;
  * Created by arunk_000 on 4/8/2015.
  */
 public class MySectionArrayAdapter extends ArrayAdapter<Section> {
-    /**
-     * Constructor
-     *
-     * @param context  The current context.
-     * @param textViewResourceId The resource ID for a layout file containing a TextView to use when creating view
-     */
-    public MySectionArrayAdapter(Context context, int textViewResourceId) {
-        super(context, textViewResourceId);
-    }
+
+    private Context context;
+    private boolean showDeleteButton = false;
 
     /**
      * Constructor
@@ -33,6 +30,11 @@ public class MySectionArrayAdapter extends ArrayAdapter<Section> {
      */
     public MySectionArrayAdapter(Context context, int resource, ArrayList<Section> items) {
         super(context, resource, items);
+        this.context = context;
+    }
+
+    public void setDeleteButtonVisibility(boolean setVisibility){
+        this.showDeleteButton = setVisibility;
     }
 
     @Override
@@ -61,6 +63,8 @@ public class MySectionArrayAdapter extends ArrayAdapter<Section> {
             TextView timesText = (TextView) v.findViewById(R.id.sectionMeetingTimes);
             TextView sectionIDText = (TextView) v.findViewById(R.id.sectionID);
             TextView designationText = (TextView) v.findViewById(R.id.sectionDesignation);
+
+            Button deleteSection = (Button) v.findViewById(R.id.section_delete_button);
 
             courseText.setTextColor(Color.BLACK);
 
@@ -132,6 +136,35 @@ public class MySectionArrayAdapter extends ArrayAdapter<Section> {
                     break;
 
             }
+
+            if(showDeleteButton){
+                deleteSection.setVisibility(View.VISIBLE);
+
+                final int itemPosition = position;
+
+                deleteSection.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AlertDialog.Builder confirmDelete = new AlertDialog.Builder(context);
+                        confirmDelete.setTitle("Are you sure you want to delete this?");
+                        confirmDelete.setPositiveButton("DELETE", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                remove(getItem(itemPosition));
+                            }
+                        });
+                        confirmDelete.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        confirmDelete.show();
+                    }
+                });
+            }
+            else
+                deleteSection.setVisibility(View.GONE);
         }
         return v;
 
